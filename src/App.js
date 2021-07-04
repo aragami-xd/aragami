eea621b06dfa3307d2491371334339ad7556bb43
cody-bui <cody.va.bui@gmail.com>
2021-07-04 17:51:44 +0930

    use semantic UI probably?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Header, Button, Icon } from 'semantic-ui-react';
+
+const pages = [
+  { title: 'Welcome', detail: 'About me' },
+  { title: 'Development', detail: 'All projects' },
+  { title: 'Speedruns', detail: 'My records' },
+  { title: 'Keyboards', detail: 'The collection' },
+  { title: 'Bodybuilding', detail: 'Training routine' }
+];
+
+function PageIntro(props) {
+  return (
+    <div>
+      <Header as='h1'>{props.page.title}</Header>
+      <Button basic color='orange' onClick={props.onNext}>
+        Next
+        <Icon name='angle double right' />
+      </Button>
+      <Button basic color='teal' onClick={props.onDetail}>{props.page.detail}</Button>
+    </div>
+  );
+}
 
 function App() {
+  const [page, setPage] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PageIntro page={pages[page]} onNext={() => setPage(page < pages.length - 1 ? page + 1 : 0)} />
     </div>
   );
 }
